da6628c2cc901eb6a18b86253e88d7e3f5f79ea2
Robert Kerr <robertcrawleykerr@gmail.com>
2016-06-30 16:26:10 -0500

    Changed variables to count backwars from 100 to -10

---
+++ do_while.php
@@ -0,0 +1,9 @@
+<?php
+
+$bytwos = 100;
+
+do {
+	echo "$bytwos" . PHP_EOL;
+	$bytwos-=5;
+	
+} while ($bytwos >= -10);
--- test.php
+++ test.php
@@ -1,23 +1,7 @@
 <?php
 
-echo 'Welcome to Codeup!' . PHP_EOL;
-echo 'This is my first PHP script!' . PHP_EOL;
-$itsHot = true;
-$youreOutside = false;
-
-if ($itsHot && $youreOutside)
-{
-	echo "sweat" . PHP_EOL;
-
-} 
-else if (!$itsHot && !$youreOutside)
-{
-	echo "enjoy the cookies" . PHP_EOL;
-
-} 
-else 
-{
-	echo "enjoy the ac" . PHP_EOL;
+$num = 5;
+while ($num <= 15){
+	echo "Num is equal to $num.", PHP_EOL;
+	$num++;
 }
-
-var_dump((true || false) ? 'it was true' : 'it was false');
