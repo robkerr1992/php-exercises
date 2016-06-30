53fe67c0d7a62aea4038d85e40446d3337f69b4d
Robert Kerr <robertcrawleykerr@gmail.com>
2016-06-30 15:07:20 -0500

    More PHP exercises - if/ifelse/ternary statements

--- test.php
+++ test.php
@@ -2,3 +2,22 @@
 
 echo 'Welcome to Codeup!' . PHP_EOL;
 echo 'This is my first PHP script!' . PHP_EOL;
+$itsHot = true;
+$youreOutside = false;
+
+if ($itsHot && $youreOutside)
+{
+	echo "sweat" . PHP_EOL;
+
+} 
+else if (!$itsHot && !$youreOutside)
+{
+	echo "enjoy the cookies" . PHP_EOL;
+
+} 
+else 
+{
+	echo "enjoy the ac" . PHP_EOL;
+}
+
+var_dump((true || false) ? 'it was true' : 'it was false');
