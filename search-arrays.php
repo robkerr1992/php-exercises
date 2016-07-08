c01b3bc17833a0fd354684d464b4978176e7c021
Robert Kerr <robertcrawleykerr@gmail.com>
2016-07-08 14:24:58 -0500

    completed step 3 without using array search

--- search-arrays.php
+++ search-arrays.php
@@ -3,3 +3,30 @@
 $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
+
+$query = 'Tina';
+//
+//function search($searchTerm, $arrayToSearch)
+//{
+//    $result = array_search($searchTerm, $arrayToSearch);
+//    if ($result !== false)
+//    {
+//        echo "true\n";
+//        return true;
+//    };
+//};
+//search($query, $names);
+
+$i = 0;
+$count = 0;
+foreach($names as $name){
+    if ($name == $compare[$i]){
+        $count++;
+        echo "Success!\n";
+    } elseif ($i == (count($compare)-1)){
+        echo "$count Matching Indices\n";
+    }
+    $i++;
+
+
+};
