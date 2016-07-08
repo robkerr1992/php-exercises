9957f8b8c380561ea9828bc97761a6787e52394c
Robert Kerr <robertcrawleykerr@gmail.com>
2016-07-08 14:36:25 -0500

    completed #3 using the array_search();

--- search-arrays.php
+++ search-arrays.php
@@ -5,28 +5,31 @@ $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
 $query = 'Tina';
-//
-//function search($searchTerm, $arrayToSearch)
-//{
-//    $result = array_search($searchTerm, $arrayToSearch);
-//    if ($result !== false)
-//    {
-//        echo "true\n";
-//        return true;
-//    };
-//};
-//search($query, $names);
+
+function search($searchTerm, $arrayToSearch)
+{
+    $result = array_search($searchTerm, $arrayToSearch);
+    if ($result !== false)
+    {
+        echo "true\n";
+        return true;
+    };
+};
+search($query, $names);
 
 $i = 0;
 $count = 0;
 foreach($names as $name){
-    if ($name == $compare[$i]){
+    if (array_search($name, $compare) !== false){
         $count++;
-        echo "Success!\n";
-    } elseif ($i == (count($compare)-1)){
-        echo "$count Matching Indices\n";
+
     }
-    $i++;
-
-
+//    if ($name == $compare[$i]){
+//        $count++;
+//        echo "Success!\n";
+//    } elseif ($i == (count($compare)-1)){
+//        echo "$count Matching Indices\n";
+//    }
+//    $i++;
 };
+echo "$count\n";
