f2a2d622788644dfa5a0d8633fe0d2e989e594a4
Robert Kerr <robertcrawleykerr@gmail.com>
2016-07-11 11:47:42 -0500

    completed assignment with optional alphabetical sort feature

--- famousphysicists.php
+++ famousphysicists.php
@@ -1,4 +1,37 @@
 <?php
+$physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+
+function humanizedlist($string, $alphabetical = false)
+{
+    $famousFakePhysicists = "";
+    $physicists = explode(', ', $string);
+    $last = end($physicists);
+//    var_dump($last);
+
+    if ($alphabetical == true){
+        sort($physicists);
+    }
+
+
+    foreach ($physicists as &$physicist) {
+        if ($physicist == $last) {
+            $physicist = "and " . $physicist . ".";
+        }
+        $famousFakePhysicists = implode(", ", $physicists);
+
+    }
+//    var_dump($physicists);
+    return $famousFakePhysicists;
+
+}
+
+
+$famousFakePhysicists =  humanizedlist($physicistsString, true);
+
+
+
+echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}\n";
+
 /**
  * Created by PhpStorm.
  * User: MacBookPro
