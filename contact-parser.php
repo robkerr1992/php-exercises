9fd148a63068dc36b1cfa90d5a2e25b273268b32
Robert Kerr <robertcrawleykerr@gmail.com>
2016-07-11 15:12:58 -0500

    completed exercise with dashes between numbers

---
+++ contact-parser.php
@@ -0,0 +1,31 @@
+<?php
+
+function parseContacts($filename)
+{
+
+    $filename = 'contacts.txt';
+
+    $contents = trim(file_get_contents($filename));
+
+//    $handle = fopen($filename, 'r');  ^^^^^^same as line above^^^^^^^
+//    $contents = trim(fread($handle, filesize($filename))); ^^^^^^^^^^^^^^^^^^
+//    fclose($handle); ^^^^^^^^^^^^^^
+    $contacts = explode("\n",$contents);
+//    array_pop($contacts); /////accomplished by trim in line 8////////
+    foreach($contacts as $contact){
+        $information = explode("|", $contact);
+        $information[1] = substr($information[1],0,3) . "-" . substr($information[1],3,3) . "-" . substr($information[1],6,4);
+        $person = [
+            'name' => $information[0],
+            'phone number' => $information[1]
+        ];
+        $finalArray[] = $person;
+
+
+    }
+
+
+    return $finalArray;
+}
+
+var_dump(parseContacts('contacts.txt'));
