ea520b320265e32f2e899b8ef33a6b056edf0324
Robert Kerr <robertcrawleykerr@gmail.com>
2016-07-14 09:29:24 -0500

    inital commit

---
+++ remove-vowels.php
@@ -0,0 +1,16 @@
+<?php
+
+$takeMeVowels = ['a','b','c','d','e','f','g','h' ];
+
+foreach($takeMeVowels as $letter){
+    if ($letter == 'a' && 'e' && 'i' && 'o' && 'u' && 'y') {}
+    unset($letter);
+}
+var_dump($takeMeVowels);
+
+/**
+ * Created by PhpStorm.
+ * User: MacBookPro
+ * Date: 7/14/16
+ * Time: 9:04 AM
+ */
