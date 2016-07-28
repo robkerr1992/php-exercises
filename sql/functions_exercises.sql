4946e77ae7cb429528f1126b2647d5a904d315a2
Robert Kerr <robertcrawleykerr@gmail.com>
2016-07-28 10:47:16 -0500

    finished fucntions exercise

--- sql/functions_exercises.sql
+++ sql/functions_exercises.sql
@@ -1,18 +1,8 @@
-SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
-SELECT * FROM employees WHERE last_name LIKE 'E%';
-SELECT * FROM employees WHERE hire_date LIKE '199%';
-SELECT * FROM employees WHERE birth_date LIKE '%%%%-12-25';
-SELECT * FROM employees WHERE last_name LIKE '%q%';
-SELECT * FROM employees WHERE first_name IN ('Irena') OR first_name IN ('Vidya') OR first_name IN ('Maya');
+SELECT COUNT(*) FROM employees WHERE (first_name IN ('Irena') OR first_name IN ('Vidya') OR first_name IN ('Maya')) AND gender IN ('M') ORDER BY first_name ASC;
+SELECT COUNT(*) FROM employees WHERE (first_name IN ('Irena') OR first_name IN ('Vidya') OR first_name IN ('Maya')) AND gender IN ('F') ORDER BY first_name ASC;
+SELECT CONCAT(first_name, ' ', last_name) as 'full_name' FROM employees WHERE last_name LIKE ('e%e');
+SELECT DATEDIFF(hire_date, CURDATE()) as 'days_working_at_company' FROM employees WHERE hire_date LIKE ('199%') AND birth_date LIKE ('%%%%-12-25') ORDER BY birth_date DESC, hire_date ASC;
+SELECT CONCAT(first_name,' ' ,last_name) as full_name, count(*) FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY full_name;
 
 
--- ORDER BY ----------
-
-
-SELECT * FROM employees WHERE (first_name IN ('Irena') OR first_name IN ('Vidya') OR first_name IN ('Maya')) AND gender IN ('M') ORDER BY first_name ASC;
-SELECT * FROM employees WHERE (first_name IN ('Irena') OR first_name IN ('Vidya') OR first_name IN ('Maya')) AND gender IN ('M') ORDER BY last_name ASC, first_name ASC;
-SELECT * FROM employees WHERE last_name LIKE ('E%') OR last_name LIKE ('%E') ORDER BY emp_no DESC;
-SELECT * FROM employees WHERE last_name LIKE ('E%') AND last_name LIKE ('%E');
-SELECT * FROM employees WHERE hire_date LIKE ('199%') AND birth_date LIKE ('%%%%-12-25') ORDER BY birth_date DESC, hire_date ASC;
-SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
 
