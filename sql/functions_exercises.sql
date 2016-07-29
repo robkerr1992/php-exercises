6021a0a3bb1b74268e1f6bae45c37ebedf5262bf
Robert Kerr <robertcrawleykerr@gmail.com>
2016-07-29 14:29:14 -0500

    completed problem 1

--- sql/functions_exercises.sql
+++ sql/functions_exercises.sql
@@ -86,4 +86,14 @@ FROM departments dept;
 
 
 
+SELECT CONCAT(first_name, ' ', last_name) as 'name'
+FROM employees
+WHERE hire_date IN(
+    SELECT hire_date
+    FROM employees WHERE emp_no = '101010'
+);
+
+
+
+
 
