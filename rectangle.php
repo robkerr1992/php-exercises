47a47582adbedfc136134c62afe4141b50b8d8a7
Robert Kerr <robertcrawleykerr@gmail.com>
2016-08-04 14:26:40 -0500

    created rectangle

---
+++ rectangle.php
@@ -0,0 +1,18 @@
+<?php
+
+class Rectangle
+{
+    public $height;
+    public $width;
+
+    public function __construct($height, $width) {
+        $this->$height = $height;
+        $this->$width = $width;
+
+
+    }
+    public function area() {
+        return $this->$height * $this->$width;
+    }
+
+}
