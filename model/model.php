dbff1330ff184373743f437d57993acff6a0a61a
Robert Kerr <robertcrawleykerr@gmail.com>
2016-08-05 14:39:55 -0500

    finished exercise

--- model/model.php
+++ model/model.php
@@ -2,5 +2,33 @@
 
 class Model
 {
+    private $data = [];
 
+    /**
+     * @return array
+     */
+    public function __set($name, $value)
+    {
+        $this->data[$name] = $value;
+
+    }
+    public function __get($name)
+    {
+        if(array_key_exists($name, $this->data)) {
+            return $this->data[$name];
+
+        }
+        return null;
+
+    }
 }
+
+$model = new Model();
+
+$model->name = 'Blake';
+$model->age = 46;
+$model->gender = 'M';
+
+echo $model->name . PHP_EOL;
+echo $model->age . PHP_EOL;
+echo $model->gender . PHP_EOL;
